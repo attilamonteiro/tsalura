305896015da8b1bf7ce1ebfdac355b0a4adabda5
Attila <attila.abreu@leste.com>
2024-07-11 18:33:05 -0300

    first commit

---
+++ src/controller/PetController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import TipoPet from "../tipos/TipoPet";
+
+let listaDePets: TipoPet[] = [];
+
+export default class PetController {
+    criaPet(req: Request, res: Response) {
+        const {id,adotado,especie,idade,nome} = <TipoPet>req.body;
+        const novoPet: TipoPet = {id,adotado,especie,idade,nome};
+        listaDePets.push(novoPet);
+        return res.status(201).json(novoPet);
+  }
+}
---
+++ src/tipos/TipoPet.ts
@@ -0,0 +1,9 @@
+export default class TipoPet {
+    id: number;
+    nome: string;
+    especie: string;
+    adotado: boolean;
+    idade: number;
+    }
+
+//expport default TipoPet;
